8462f90e0f158ec11187009dd45a8ed4f33171dc
zhoudaxia <muzhouz@gmail.com>
2024-05-16 23:11:59 +0800

    feat: 避免重复导入

--- src/pages/manager/index.tsx
+++ src/pages/manager/index.tsx
@@ -1,15 +1,22 @@
 import './index.less'
 import React, {useCallback, useEffect, useRef, useState} from 'react'
-import {Input} from 'antd'
+import {Input, notification} from 'antd'
 import {PlusOutlined, SearchOutlined} from '@ant-design/icons'
 import {getBook} from '~/utils/reader'
 import iddb from '~/storage/iddb'
 import {toArrayBuffer} from '~/utils/fileReader'
 import BookCard from './BookCard'
 import Storage from '~/storage/localStorage'
+import CryptoJS from 'crypto-js'
+
+function getMd5(data) {
+  const hash = CryptoJS.MD5(CryptoJS.lib.WordArray.create(data))
+  return hash.toString(CryptoJS.enc.Hex)
+}
 
 export default function Manager() {
   const [books, setBooks] = useState<any[]>([])
+  const [notice, contextHolder] = notification.useNotification();
   const refFileInput = useRef<HTMLInputElement>(null)
   const refBookUserInfo = useRef<any>()
 
@@ -28,15 +35,37 @@ export default function Manager() {
   }, [])
 
   const handleFileChange = useCallback(async (e) => {
+    const md5Set = new Set(books.map(_ => _.md5))
+    const duplicateFiles = []
+    console.log('zz_debug', md5Set, books)
     await Promise.all([...e.target.files].map(async file => {
       const book: any = await getBook(file)
-      const cover = await book.getCover()
       const data = await toArrayBuffer(file)
+      const md5 = getMd5(data)
+      if (md5Set.has(md5)) {
+        duplicateFiles.push(file.name)
+        return
+      }
+      const cover = await book.getCover()
       const createTime = Date.now()
-      await iddb.addBook(data, {createTime, cover, name: file.name, type: file.type, ...book.metadata})
+      await iddb.addBook(data, {createTime, cover, name: file.name, type: file.type, md5, ...book.metadata})
     }))
+    const bookNum = e.target.files.length
+    const duplicateNum = duplicateFiles.length
+    if (duplicateFiles.length > 0) {
+      notice.warning({
+        message: `成功导入${bookNum - duplicateNum}本，有重复书籍(${duplicateNum}本)：`,
+        description: duplicateFiles.map(_ => <div>{_}</div>),
+        duration: 3,
+      })
+    } else {
+      notice.success({
+        message: `成功导入${bookNum}本书籍`,
+        duration: 1,
+      })
+    }
     loadBooks()
-  }, [])
+  }, [books, notice])
 
   useEffect(() => {
     refBookUserInfo.current = new Storage('book-userinfo')
@@ -50,6 +79,7 @@ export default function Manager() {
 
   return (
     <div className="manager">
+      {contextHolder}
       <div className="manager-header">
         <input ref={refFileInput} className="file-input" multiple type="file" accept=".epub,.mobi,.azw3,fb2,cbz" onChange={handleFileChange}/>
         <Input className="search-input"
