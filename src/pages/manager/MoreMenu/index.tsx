76e00c35097508fdd7c1e0d80f14c8527f7abc18
zhoudaxia <muzhouz@gmail.com>
2024-05-25 21:21:17 +0800

    feat: 移动端只允许导入导出配置

--- src/pages/manager/MoreMenu/index.tsx
+++ src/pages/manager/MoreMenu/index.tsx
@@ -5,16 +5,21 @@ import Backup from './Backup'
 import Restore from './Restore'
 import BackupConfig from './BackupConfig'
 import RestoreConfig from './RestoreConfig'
+import {isMobile} from '~/utils/userAgent'
 
 export default function MoreMenu({onRestoreComplete, getBookUserInfo, getMd5Set}) {
   const [notice, contextHolder] = notification.useNotification()
   const menuItems = [
-    {label: <Backup getBookUserInfo={getBookUserInfo}/>, key: 1},
-    {label: <Restore notice={notice} onComplete={onRestoreComplete}
-              getBookUserInfo={getBookUserInfo} getMd5Set={getMd5Set}/>, key: 2},
     {label: <BackupConfig getBookUserInfo={getBookUserInfo}/>, key: 3},
     {label: <RestoreConfig getBookUserInfo={getBookUserInfo} onComplete={onRestoreComplete}/>, key: 4},
   ]
+  if (!isMobile) {
+    menuItems.push(...[
+      {label: <Backup getBookUserInfo={getBookUserInfo}/>, key: 1},
+      {label: <Restore notice={notice} onComplete={onRestoreComplete}
+        getBookUserInfo={getBookUserInfo} getMd5Set={getMd5Set}/>, key: 2},
+    ])
+  }
   return (
     <>
       {contextHolder}
